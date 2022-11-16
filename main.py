5064f3051bbdc82893c256d5a1c9a5613e625438
Fyn! <113708207+Moodyxfyn@users.noreply.github.com>
2022-11-16 11:13:39 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#Intro to the game.
+
+print('Welcome to, Console Mandarin HSK 1 **MOCK TEST**')
+
+#make the player type their name
+
+playing = input ("Please Type Your Name. ")
+print (playing)
+yes_no = input ('Is this your name? ')
+if yes_no == 'yes':
+    ()
+else:
+    playing = input ('Please type your name! ')
+
+#now here is where the test begins.
+
+test = input ('What does 再见 mean? ')
+if test == 'goodbye':
+    ()
+else:
+   print ('sorry. the anwser is, goodbye')
